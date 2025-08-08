307ec4f535ce4ceea01db7bb5994cfd7116485ea
Maawiah  Qaiser <maviaqaiser09@gmail.com>
2025-08-08 14:21:56 +0500

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,20 @@
+#
+print('Hello world')
+
+# A variable is a name given to a memory location in a program.
+name = "Shradha"
+age = 23
+price = 25.99
+
+# Data types
+# |-Integers
+# |-String 
+# |-Float
+# |-Boolean
+# |-None
+#
+print(type(name))  # <class 'str'>
+print(type(age))   # <class 'int'>
+print(type(price)) # <class 'float'>
+print(type(True))  # <class 'bool'>
+print(type(None))  # <class 'NoneType'>
